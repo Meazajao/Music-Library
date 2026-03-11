31249da8750cb3271b2d11192f87faa19212de41
Joeri8 <erikssonj97@gmail.com>
2026-03-11 13:54:25 +0100

    tsconfig och serverstart

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import express from "express";
 import cors from "cors";
+
 import artistRoutes from "./routes/artistRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/artists", artistRoutes);
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("Music library API is running!");
+  res.send("Music Library API running");
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server running on port 3000");
 });
