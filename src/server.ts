06400b887e02ac01c72b1c61230c871c49ed7cf3
Joeri8 <erikssonj97@gmail.com>
2026-03-11 12:26:28 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
+import artistRoutes from "./routes/artistRoutes";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/artists", artistRoutes);
 
 app.use(cors());
 app.use(express.json());
