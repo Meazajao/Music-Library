04ef62bfc07d1a04233be2b34010ce3e4389d9ba
Joeri8 <erikssonj97@gmail.com>
2026-03-25 13:07:31 +0100

    Frondend / startsida - about - contact
    Gjort startsidan - contact - about och la till en main.js som ''pratar'' med api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,6 @@
-//api
+const API_BASE_URL = "http://localhost:3000";
+
+async function getTopArtists() {
+  const response = await fetch(`${API_BASE_URL}/artists/top3`);
+  return response.json();
+}
