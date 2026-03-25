ed23b902770d7c3d2acaa5384b4430f7ca28b360
Meazajao <153733325+Meazajao@users.noreply.github.com>
2026-03-25 14:09:22 +0100

    get artist by id

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getArtists, getTop3Artists } from "../services/artistService";
+import { getArtists, getTop3Artists, getArtist } from "../services/artistService";
 
 export const getAllArtists = async (req: Request, res: Response) => {
   try {
@@ -14,6 +14,21 @@ export const getTopArtists = async (req: Request, res: Response) => {
   try {
     const artists = await getTop3Artists();
     res.json(artists);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const getArtistById = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const artist = await getArtist(id);
+
+    if (!artist) {
+      return res.status(404).json({ message: "Artist not found" });
+    }
+
+    res.json(artist);
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
--- src/services/artistService.ts
+++ src/services/artistService.ts
@@ -1,11 +1,22 @@
-import {db} from "../config/mysql";
+import { db } from "../config/mysql";
 
 export const getArtists = async () => {
-    const [artists] = await db.query("SELECT * FROM artists");
-    return artists;
-  };
-  
-  export const getTop3Artists = async () => {
-    const [artists] = await db.query("SELECT * FROM artists LIMIT 3");
-    return artists;
-  };
+  const [artists] = await db.query("SELECT * FROM artists");
+  return artists;
+};
+
+export const getTop3Artists = async () => {
+  const [artists] = await db.query(
+    "SELECT * FROM artists ORDER BY play_count DESC LIMIT 3"
+  );
+  return artists;
+};
+
+export const getArtist = async (id: number) => {
+  const [rows]: any = await db.query(
+    "SELECT * FROM artists WHERE id = ?",
+    [id]
+  );
+
+  return rows[0];
+};
