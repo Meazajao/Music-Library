5e44c6a00251bc5f8bec20e7e7bdba566a4e391b
Joeri8 <erikssonj97@gmail.com>
2026-04-02 11:06:19 +0200

    Komplitering för PUT

--- src/routes/artistRoutes.ts
+++ src/routes/artistRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllArtists, getTopArtists, getArtistById } from "../controllers/artistController";
+import { getAllArtists, getTopArtists, getArtistById, updateArtist } from "../controllers/artistController";
 
 const router = express.Router();
 
 router.get("/", getAllArtists);
 router.get("/top3", getTopArtists);
 router.get("/:id", getArtistById);
+router.put("/:id", updateArtist);
 
 export default router;
